aed728944bcefc28c17d137a7aea6129e6152fee
SG master <1363028533@qq.com>
2018-05-03 22:56:27 +0800

    Init.

---
+++ WordMember/app/src/main/java/supergp/wordmember/quiz/SingleTest.java
@@ -0,0 +1,16 @@
+package supergp.wordmember.quiz;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import supergp.wordmember.R;
+
+public class SingleTest extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+
+    }
+}
