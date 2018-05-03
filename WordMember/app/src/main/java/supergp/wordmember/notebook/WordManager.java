5252cd31b43af5154b89d465073897825491a130
SG master <1363028533@qq.com>
2018-05-03 22:56:05 +0800

    Init.

---
+++ WordMember/app/src/main/java/supergp/wordmember/notebook/WordManager.java
@@ -0,0 +1,16 @@
+package supergp.wordmember.notebook;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import supergp.wordmember.R;
+
+public class WordManager extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note);
+
+    }
+}
