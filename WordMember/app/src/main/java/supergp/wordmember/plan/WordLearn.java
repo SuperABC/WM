b9063f0bb8db60e9b372a7b3eeef066f7267cb0a
SG master <1363028533@qq.com>
2018-05-03 22:56:17 +0800

    Init.

---
+++ WordMember/app/src/main/java/supergp/wordmember/plan/WordLearn.java
@@ -0,0 +1,16 @@
+package supergp.wordmember.plan;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import supergp.wordmember.R;
+
+public class WordLearn extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_learn);
+
+    }
+}
