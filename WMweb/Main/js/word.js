553ab30d45b9134c0b41e909ca03c5e1b894c00a
SG master <1363028533@qq.com>
2018-05-26 23:00:37 +0800

    Build ui frame.

--- WMweb/Main/js/word.js
+++ WMweb/Main/js/word.js
@@ -2,3 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.datepicker');
     var instances = M.Datepicker.init(elems, {});
 });
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems, {});
+});
