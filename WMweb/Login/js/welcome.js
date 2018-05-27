1dcd47239b920f0f2fcef5927a104d166f225d13
SG master <1363028533@qq.com>
2018-05-27 09:28:46 +0800

    Welcome page jump.

--- WMweb/Login/js/welcome.js
+++ WMweb/Login/js/welcome.js
@@ -1,9 +1,12 @@
 function login(){
     window.open('../Main/user.html');
+    window.close();
 }
 function signup(){
     window.open('sign.html');
+    window.close();
 }
 function tourvisit(){
     window.open('search.html');
+    window.close();
 }
