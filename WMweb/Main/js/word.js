816ac8ecd9d5f9c15d7af8e7e460addc853e5129
SG master <1363028533@qq.com>
2018-06-02 10:08:26 +0800

    Implement word learning.

--- WMweb/Main/js/word.js
+++ WMweb/Main/js/word.js
@@ -1,13 +1,30 @@
-let id = window.location.href.split('?');
-if(id.length > 1) {
-    id = id[1].split('&')[0];
+let paras = window.location.href.split('?');
+let id = "";
+if(paras.length > 1) {
+    paras = paras[1].split('&');
+    id = paras[0];
     if (id.split('=')[0] !== "id") id = "";
 
     if(document.getElementById("userid"))
         document.getElementById("userid").innerText = 'User:' + window.location.href.split('?')[1].split('=')[1];
+
+    if(paras.length>1){
+        if(paras[1].split('=')[0] === "word"){
+            $.ajax({
+                data: "word_search&" + paras[1].split('=')[1],
+                url: 'http://127.0.0.1:4497',
+                dataType: "jsonp",
+                jsonp: "callback",
+                jsonpCallback: "success_jsonpCallback"
+            }).done(function (res) {
+                let tmp = JSON.parse(res);
+                document.getElementById("title").innerText=tmp.word;
+                document.getElementById("trans").innerText=tmp.trans;
+                document.getElementById("examp").innerText=tmp.examp;
+            });
+        }
+    }
 }
-
-
 
 document.addEventListener('DOMContentLoaded', function() {
     let elems = document.querySelectorAll('.datepicker');
@@ -25,9 +42,18 @@ function logOut(){
     window.location.href='../Login/welcome.html';
 }
 
-function singleSearch(word){
-
+function queryWord(){
+    alert(document.getElementById("input").value);
+    $.ajax({
+        data: "word_search&" + document.getElementById("input").value,
+        url: 'http://127.0.0.1:4497',
+        dataType: "jsonp",
+        jsonp: "callback",
+        jsonpCallback: "success_jsonpCallback"
+    }).done(function (res) {
+        if(JSON.parse(res).length) alert(res);
+        else alert(JSON.parse(res).word);
+    });
 }
-function multiSearch(word){
 
-}
+
--- WMweb/Server/NodeServer/index.js
+++ WMweb/Server/NodeServer/index.js
@@ -14,6 +14,20 @@ let connection = mysql.createConnection({
     port: '3306'
 });
 
+let fs = require("fs");
+let dict = new Buffer(10436608);
+let words = [];
+fs.open("src/dict.all", "r", function(err, fd){
+    fs.read(fd, dict, 0, dict.length, 0, function(err, bytes){
+        let entries = dict.toString().split('####');
+        entries.forEach(function(item){
+            let cont = item.split('$$$$');
+            words.push({word:cont[0], trans:cont[1], examp:cont[2]});
+        });
+        console.log("read finished.");
+    });
+});
+
 function handle(data, res){
     if(data[1]==="login_req"){
         let queryStr = 'select * from users where id = \'' + data[2] + '\' and password = \'' + data[3] + '\';';
@@ -51,6 +65,86 @@ function handle(data, res){
         let json = JSON.stringify('success');
         res.end('success_jsonpCallback(' + json + ')');
     }
+    else if(data[1]==="word_search"){
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        let json = JSON.stringify(wordSearch(data[2]));
+        res.end('success_jsonpCallback(' + json + ')');
+    }
+    else if(data[1]==="find_plan"){
+        let queryStr = 'select wordset from milestone where id = \'' + data[2] + '\';';
+        connection.query(queryStr, function(err, rows) {
+            if (err) console.log(err);
+            if(rows.length>0) {
+                res.writeHead(200, {'Content-Type': 'application/json'});
+                let json = JSON.stringify(rows[0].milestone);
+                res.end('success_jsonpCallback(' + json + ')');
+            }
+        });
+    }
+    else if(data[1]==="add_plan"){
+        let queryStr = 'insert into milestone values(\'' + data[2] + '\', \'' + data[2] + '\', 0, 8);';
+        connection.query(queryStr, function(err, rows) {
+            if (err) console.log(err);
+            res.writeHead(200, {'Content-Type': 'application/json'});
+            let json = JSON.stringify("success");
+            res.end('success_jsonpCallback(' + json + ')');
+        });
+    }
+    else if(data[1]==="fetch_plan"){
+        let queryStr1 = 'select wordset, base from milestone where id = \'' + data[2] + '\';';
+        connection.query(queryStr1, function(err, wordbase) {
+            if (err) console.log(err);
+            if(wordbase.length>0) {
+                let queryStr2 = 'select word from ' + wordbase[0].wordset + ' where num >= ' + wordbase[0].base + ' and num < ' + wordbase[0].base + '+8;';
+                connection.query(queryStr2, function(err, contents) {
+                    if (err) console.log(err);
+                    if(contents.length>0) {
+                        console.log(contents);
+                        res.writeHead(200, {'Content-Type': 'application/json'});
+                        let json = JSON.stringify(contents);
+                        res.end('success_jsonpCallback(' + json + ')');
+                    }
+                });
+            }
+        });
+    }
+}
+
+function searchMeet(iter, feed){
+    if(feed==="*")return true;
+    if(feed.length===0&&iter.length===0)return true;
+    if(feed.indexOf("*")===0){
+        while(!iter.length===0){
+            if(searchMeet(iter = iter.substr(1), feed.substr(1)))
+                return true;
+        }
+    }
+    if(iter.length===0) return false;
+    if(feed.indexOf("?")===0 || feed[0]===iter[0]){
+        return searchMeet(iter.substr(1), feed.substr(1));
+    }
+}
+
+function wordSearch(word){
+    if(word.indexOf('*')>=0||word.indexOf('?')>=0)return multiSearch(word.toLowerCase());
+    return singleSearch(word.toLowerCase(), 0, words.length);
+}
+function singleSearch(word, begin, end){
+    if(begin>=end-1){
+        if(word===words[begin].word)return JSON.stringify(words[begin]);
+        return JSON.stringify("");
+    }
+    if(word===words[Math.floor((begin+end)/2)].word.toLowerCase())return JSON.stringify(words[Math.floor((begin+end)/2)]);
+    else if(word > words[Math.floor((begin+end)/2)].word.toLowerCase())return singleSearch(word, Math.floor((begin+end)/2), end);
+    else return singleSearch(word, begin, Math.floor((begin+end)/2));
+}
+function multiSearch(word){
+    let res = [];
+    words.forEach(function(item){
+        if(searchMeet(item.word, word))
+            res.push(item);
+    });
+    return JSON.stringify(res);
 }
 
 
